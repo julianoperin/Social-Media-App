83ce0fb7aa7e9094d77ae5d7402e4a2623730a13
‘Juliano <julianoperins@gmail.com>
2021-06-27 23:01:28 -0400

    Created the first route

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("helmet");
+const helmet = require("helmet");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/users");
 
-// dotenv.config();
+dotenv.config();
+
+mongoose.connect(
+  "process.env.MONGO_URL",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("MongoDB Connected!!!");
+  }
+);
 
 app.get("/", (req, res) => {
-  res.send("It's working!");
+  res.send("Home Page!");
 });
+
+app.get("/users", (req, res) => {
+  res.send("Users Page!");
+});
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/user", userRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
