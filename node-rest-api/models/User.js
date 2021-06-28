b36ee442f7654f7fd4447d33a823ea2dd55b8a02
‘Juliano <julianoperins@gmail.com>
2021-06-28 11:01:23 -0400

    created the register/login/update routes

--- node-rest-api/models/User.js
+++ node-rest-api/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      require: true,
       min: 3,
       max: 20,
       unique: true,
@@ -32,7 +32,7 @@ const UserSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
-    followins: {
+    followings: {
       type: Array,
       default: [],
     },
@@ -40,10 +40,24 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
+    },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", UserSchema);
