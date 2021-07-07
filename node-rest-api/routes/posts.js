c4235011454e3c8ff2b2225b19715e4f07e30e33
‘Juliano <julianoperins@gmail.com>
2021-07-06 23:17:38 -0400

    added the get post and timeline routes

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -60,15 +60,32 @@ router.put("/:id/like", async (req, res) => {
     res.status(500).json(err);
   }
 });
-// //get a post
+//get a post
 
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const post = await Post.findById(req.params.id);
-//     res.status(200).json(post);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get timeline posts
+
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
